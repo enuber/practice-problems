cb9198f4047648c972f1f6981b8127506dcdc4ac
Erik Nuber <erik.nuber@yahoo.com>
2017-03-06 12:40:08 -0800

    randomize array finished

--- practice20/answer.js
+++ practice20/answer.js
@@ -1,1 +1,24 @@
-answer.js
+function randomizeList(inputArr) {
+    var outputArr = [];
+    if (inputArr.length > 0) {
+        var originalArrLength = inputArr.length;
+        for (var i = 0; i < originalArrLength; i++) {
+            //inputArr.length - 1 is used because we have something at index 0, you can also take the -1 and +1
+            //out to resolve this as well.
+            var randNum = Math.floor(Math.random() * inputArr.length-1)+1;
+            outputArr.push(inputArr[randNum]);
+            inputArr.splice(randNum, 1);
+        }
+    } else {
+        return ("The Array Has Nothing In It.")
+    }
+    return outputArr;
+}
+
+var testArray =  [0,1,2,3,4,5,6,7,8,9];
+var testArray2 = [];
+
+console.log(randomizeList(testArray));
+console.log(randomizeList(testArray2));
+
+
