d80aaf51a4578ba248d99301b238b07009f8da8f
Erik Nuber <erik.nuber@yahoo.com>
2017-03-06 12:42:54 -0800

    randomize array

--- practice20/answer.js
+++ practice20/answer.js
@@ -18,6 +18,7 @@ function randomizeList(inputArr) {
 var testArray =  [0,1,2,3,4,5,6,7,8,9];
 var testArray2 = [];
 
+//testing arrays
 console.log(randomizeList(testArray));
 console.log(randomizeList(testArray2));
 
